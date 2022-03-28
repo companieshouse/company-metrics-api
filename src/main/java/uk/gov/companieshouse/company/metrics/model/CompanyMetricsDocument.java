8030fca652ec0155a999c5e8504ac5cd528f41df
sampipparampil <sampipparampil@companieshouse.gov.uk>
2022-03-28 11:02:26 +0100

    dsnd-526:Updated application yml

--- src/main/java/uk/gov/companieshouse/company/metrics/model/CompanyMetricsDocument.java
+++ src/main/java/uk/gov/companieshouse/company/metrics/model/CompanyMetricsDocument.java
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 import uk.gov.companieshouse.api.metrics.MetricsApi;
 
-@Document(collection = "#{@environment.getProperty('mongodb.company.metrics.collection.name')}")
+@Document(collection = "#{@environment.getProperty('spring.data.mongodb.collection')}")
 public class CompanyMetricsDocument {
     @Id
     @Field("_id")
