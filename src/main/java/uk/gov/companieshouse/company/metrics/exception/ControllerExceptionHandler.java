df32cb95459b4c9db875eb349dea4af046256681
igodman <igodman@companieshouse.gov.uk>
2022-04-25 14:55:51 +0100

    Check style problems fixed.

--- src/main/java/uk/gov/companieshouse/company/metrics/exception/ControllerExceptionHandler.java
+++ src/main/java/uk/gov/companieshouse/company/metrics/exception/ControllerExceptionHandler.java
@@ -1,27 +1,18 @@
 package uk.gov.companieshouse.company.metrics.exception;
 
-import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.UUID;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.DataAccessResourceFailureException;
-import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-
-import org.springframework.web.server.ResponseStatusException;
-import uk.gov.companieshouse.company.metrics.repository.charges.ChargesRepository;
-import uk.gov.companieshouse.company.metrics.repository.metrics.CompanyMetricsRepository;
 import uk.gov.companieshouse.logging.Logger;
-import uk.gov.companieshouse.logging.LoggerFactory;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
@@ -37,7 +28,7 @@ public class ControllerExceptionHandler {
         this.logger = logger;
     }
 
-    private void populateResponseBody(Map<String, Object> responseBody , String correlationId){
+    private void populateResponseBody(Map<String, Object> responseBody , String correlationId) {
         responseBody.put("timestamp", LocalDateTime.now());
         responseBody.put("message", "There is issue completing the request.");
         responseBody.put("correlationId", correlationId);
@@ -60,11 +51,18 @@ public class ControllerExceptionHandler {
         return new ResponseEntity<>(responseBody, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    /**
+     * DataAccessResourceFailureException handler.
+     *
+     * @param ex      exception to handle.
+     * @return error response to return.
+     */
     @ExceptionHandler(value = {DataAccessResourceFailureException.class})
-    public ResponseEntity<Object> handleException(DataAccessResourceFailureException ex, WebRequest request) {
-        var correlationId = generateShortCorrelationId();
+    public ResponseEntity<Object> handleException(DataAccessResourceFailureException ex,
+            WebRequest request) {
+        String correlationId = generateShortCorrelationId();
         logger.error(String.format("Started: handleException: %s Generating error response ",
-            correlationId), ex);
+                correlationId), ex);
         Map<String, Object> responseBody = new LinkedHashMap<>();
         populateResponseBody(responseBody, correlationId);
         request.setAttribute("javax.servlet.error.exception", ex, 0);
@@ -73,11 +71,18 @@ public class ControllerExceptionHandler {
         return new ResponseEntity<>(responseBody, HttpStatus.SERVICE_UNAVAILABLE);
     }
 
+    /**
+     * DataAccessException handler.
+     *
+     * @param ex      exception to handle.
+     * @return error response to return.
+     */
     @ExceptionHandler(value = {DataAccessException.class})
-    public ResponseEntity<Object> handleException(DataAccessException ex, WebRequest request) {
-        var correlationId = generateShortCorrelationId();
+    public ResponseEntity<Object> handleException(DataAccessException ex,
+            WebRequest request) {
+        String correlationId = generateShortCorrelationId();
         logger.error(String.format("Started: handleException: %s Generating error response ",
-            correlationId), ex);
+                correlationId), ex);
         Map<String, Object> responseBody = new LinkedHashMap<>();
         populateResponseBody(responseBody, correlationId);
         request.setAttribute("javax.servlet.error.exception", ex, 0);
@@ -85,8 +90,15 @@ public class ControllerExceptionHandler {
         return new ResponseEntity<>(responseBody, HttpStatus.NOT_EXTENDED);
     }
 
+    /**
+     * HttpMessageNotReadableException handler.
+     *
+     * @param ex      exception to handle.
+     * @return error response to return.
+     */
     @ExceptionHandler(value = {HttpMessageNotReadableException.class})
-    public ResponseEntity<Object> handleException(HttpMessageNotReadableException ex, WebRequest request) {
+    public ResponseEntity<Object> handleException(HttpMessageNotReadableException ex,
+            WebRequest request) {
         var correlationId = generateShortCorrelationId();
         logger.error(String.format("Started: handleException: %s Generating error response ",
                 correlationId), ex);
