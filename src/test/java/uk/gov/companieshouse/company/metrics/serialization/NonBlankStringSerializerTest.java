64da5dba2d3d9fa1b0dd850e8de1fd2940280301
adeshpandech <adeshpande@companieshouse.gov.uk>
2022-06-16 16:19:59 +0100

    DSND-1106: update private api sdk to change MetricsApi move-on data type to data-time and added requied convertor to fix the issue

---
+++ src/test/java/uk/gov/companieshouse/company/metrics/serialization/NonBlankStringSerializerTest.java
@@ -0,0 +1,49 @@
+package uk.gov.companieshouse.company.metrics.serialization;
+
+import static org.mockito.Mockito.verify;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import java.io.IOException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class NonBlankStringSerializerTest {
+
+    @Mock
+    private SerializerProvider serializerProvider;
+    @Mock
+    private JsonGenerator jsonGenerator;
+
+    private NonBlankStringSerializer stringSerializer;
+
+    @BeforeEach
+    void setUp() {
+        stringSerializer = new NonBlankStringSerializer();
+    }
+
+    @Test
+    void testSerialiseNonBlankValue() throws IOException {
+        stringSerializer.serialize("test", jsonGenerator, serializerProvider);
+
+        verify(jsonGenerator).writeString("test");
+    }
+
+    @Test
+    void testSerialiseBlankValue() throws IOException {
+        stringSerializer.serialize("   ", jsonGenerator, serializerProvider);
+
+        verify(jsonGenerator).writeNull();
+    }
+
+    @Test
+    void testSerialiseNullValue() throws IOException {
+        stringSerializer.serialize(null, jsonGenerator, serializerProvider);
+
+        verify(jsonGenerator).writeNull();
+    }
+}
