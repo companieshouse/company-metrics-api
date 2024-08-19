c1c695c8109f2a52020ebb59cf7840edd9c785d6
YaaseenR <mrahman1@companieshouse.gov.uk>
2024-08-19 10:49:51 +0100

    changed app config file

--- src/main/java/uk/gov/companieshouse/company/metrics/auth/EricTokenAuthenticationFilter.java
+++ src/main/java/uk/gov/companieshouse/company/metrics/auth/EricTokenAuthenticationFilter.java
@@ -1,34 +1,35 @@
 package uk.gov.companieshouse.company.metrics.auth;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Optional;
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang3.ArrayUtils;
+import org.springframework.lang.NonNull;
 import org.springframework.web.filter.OncePerRequestFilter;
-import uk.gov.companieshouse.company.metrics.logging.DataMapHolder;
 import uk.gov.companieshouse.logging.Logger;
 
 public class EricTokenAuthenticationFilter extends OncePerRequestFilter {
 
-    private final Logger logger;
+    private final Logger tokenLogger;
 
-    public EricTokenAuthenticationFilter(Logger logger) {
-        this.logger = logger;
+    public EricTokenAuthenticationFilter(Logger tokenLogger) {
+        this.tokenLogger = tokenLogger;
     }
 
     @Override
-    protected void doFilterInternal(jakarta.servlet.http.HttpServletRequest request,
-                                    jakarta.servlet.http.HttpServletResponse response,
-                                    jakarta.servlet.FilterChain filterChain) throws
-            jakarta.servlet.ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+
         String ericIdentity = request.getHeader("ERIC-Identity");
 
         if (StringUtils.isBlank(ericIdentity)) {
-            logger.error("Request received without eric identity", DataMapHolder.getLogMap());
+            tokenLogger.error("Request received without eric identity");
             response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
             return;
         }
@@ -37,54 +38,19 @@ public class EricTokenAuthenticationFilter extends OncePerRequestFilter {
 
         if (!("key".equalsIgnoreCase(ericIdentityType)
                 || ("oauth2".equalsIgnoreCase(ericIdentityType)))) {
-            logger.error("Request received without correct eric identity type",
-                    DataMapHolder.getLogMap());
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
-            return;
-        }
-
-        if (!isKeyAuthorised((HttpServletRequest) request, ericIdentityType)) {
-            logger.info("Supplied key does not have sufficient privilege for the action",
-                    DataMapHolder.getLogMap());
-            response.sendError(HttpServletResponse.SC_FORBIDDEN);
-            return;
-        }
-
-        filterChain.doFilter(request,response);
-
-    }
-
-    /*@Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
-            FilterChain filterChain) throws ServletException, IOException {
-
-        String ericIdentity = request.getHeader("ERIC-Identity");
-
-        if (StringUtils.isBlank(ericIdentity)) {
-            logger.error("Request received without eric identity", DataMapHolder.getLogMap());
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
-            return;
-        }
-
-        String ericIdentityType = request.getHeader("ERIC-Identity-Type");
-
-        if (!("key".equalsIgnoreCase(ericIdentityType)
-                || ("oauth2".equalsIgnoreCase(ericIdentityType)))) {
-            logger.error("Request received without correct eric identity type",
-                    DataMapHolder.getLogMap());
+            tokenLogger.error("Request received without correct eric identity type");
             response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
             return;
         }
 
         if (!isKeyAuthorised(request, ericIdentityType)) {
-            logger.info("Supplied key does not have sufficient privilege for the action",
-                    DataMapHolder.getLogMap());
+            tokenLogger.info("Supplied key does not have sufficient privilege for the action");
             response.sendError(HttpServletResponse.SC_FORBIDDEN);
             return;
         }
 
-        filterChain.doFilter(request,response);
-    }*/
+        filterChain.doFilter(request, response);
+    }
 
     private boolean isKeyAuthorised(HttpServletRequest request, String ericIdentityType) {
         String[] privileges = getApiKeyPrivileges(request);
@@ -101,6 +67,4 @@ public class EricTokenAuthenticationFilter extends OncePerRequestFilter {
                 .map(s -> s.split(","))
                 .orElse(new String[]{});
     }
-
-
 }
--- src/main/java/uk/gov/companieshouse/company/metrics/config/ApplicationConfig.java
+++ src/main/java/uk/gov/companieshouse/company/metrics/config/ApplicationConfig.java
@@ -3,24 +3,40 @@ package uk.gov.companieshouse.company.metrics.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import uk.gov.companieshouse.company.metrics.auth.EricTokenAuthenticationFilter;
-import uk.gov.companieshouse.company.metrics.logging.DataMapHolder;
-import uk.gov.companieshouse.company.metrics.logging.StructuredLoggingFilter;
+import uk.gov.companieshouse.company.metrics.converter.CompanyMetricsReadConverter;
+import uk.gov.companieshouse.company.metrics.converter.CompanyMetricsWriteConverter;
+import uk.gov.companieshouse.company.metrics.converter.EnumConverters;
+import uk.gov.companieshouse.company.metrics.serialization.LocalDateDeSerializer;
+import uk.gov.companieshouse.company.metrics.serialization.LocalDateSerializer;
 import uk.gov.companieshouse.environment.EnvironmentReader;
 import uk.gov.companieshouse.environment.impl.EnvironmentReaderImpl;
 import uk.gov.companieshouse.logging.Logger;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class ApplicationConfig extends WebSecurityConfigurerAdapter {
+@EnableMethodSecurity
+public class ApplicationConfig {
 
     @Autowired
     private Logger logger;
@@ -33,35 +49,61 @@ public class ApplicationConfig extends WebSecurityConfigurerAdapter {
     /**
      * Configure Http Security.
      */
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        this.logger.debug("Start: configure Http Security", DataMapHolder.getLogMap());
-        http.httpBasic().disable()
-                //REST APIs not enabled for cross site script headers
-                .csrf().disable() //NOSONAR
-                .formLogin().disable()
-                .logout().disable()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, Logger logger) throws Exception {
+        http.httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable)
+                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterAt(new EricTokenAuthenticationFilter(logger),
                         BasicAuthenticationFilter.class)
-                .addFilterBefore(new StructuredLoggingFilter(logger),
-                        EricTokenAuthenticationFilter.class)
-                .authorizeRequests()
-                .anyRequest().permitAll();
-        this.logger.debug("End: configure Http Security", DataMapHolder.getLogMap());
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return http.build();
     }
 
     /**
-     * Exclude health check endpoint from security filter.
+     * Configure Web Security.
      */
-    @Override
-    public void configure(WebSecurity web) {
-        this.logger.debug("Start: Excluding health check endpoint from security filter",
-                DataMapHolder.getLogMap());
-        web.ignoring().antMatchers("/company-metrics-api/healthcheck");
-        this.logger.debug("End: Excluding health check endpoint from security filter",
-                DataMapHolder.getLogMap());
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers("/company-metrics-api/healthcheck");
+    }
+
+    /**
+     * mongoCustomConversions.
+     *
+     * @return MongoCustomConversions.
+     */
+    @Bean
+    public MongoCustomConversions mongoCustomConversions() {
+        var objectMapper = mongoDbObjectMapper();
+        return new MongoCustomConversions(List.of(
+                new CompanyMetricsWriteConverter(objectMapper),
+                new CompanyMetricsReadConverter(objectMapper),
+                new EnumConverters()));
+    }
+
+    /**
+     * Mongo DB Object Mapper.
+     *
+     * @return ObjectMapper.
+     */
+    private ObjectMapper mongoDbObjectMapper() {
+        var objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+        var module = new SimpleModule();
+        module.addSerializer(LocalDate.class, new LocalDateSerializer());
+        module.addDeserializer(LocalDate.class, new LocalDateDeSerializer());
+        objectMapper.registerModule(module);
+
+        return objectMapper;
+    }
+
+    @Bean
+    public List<String> externalMethods() {
+        return Arrays.asList(HttpMethod.GET.name());
     }
 }
