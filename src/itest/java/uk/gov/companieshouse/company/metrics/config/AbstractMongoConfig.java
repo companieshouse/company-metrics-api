9b319a5557e40ca335e9efdc207696207ca3966c
johncook-ch <jcook@companieshouse.gov.uk>
2026-05-01 15:17:26 +0100

    ASM-2038: Test MongoDB 8 compatibility
    - Updated MongoDB test containers to 8
    - Tidied up POM and removed unused CVE fixes

--- src/itest/java/uk/gov/companieshouse/company/metrics/config/AbstractMongoConfig.java
+++ src/itest/java/uk/gov/companieshouse/company/metrics/config/AbstractMongoConfig.java
@@ -15,7 +15,7 @@ import org.testcontainers.utility.DockerImageName;
 public class AbstractMongoConfig {
 
     public static final MongoDBContainer mongoDBContainer = new MongoDBContainer(
-            DockerImageName.parse("mongo:7.0.17"));
+            DockerImageName.parse("mongo:8"));
 
     @DynamicPropertySource
     public static void setProperties(DynamicPropertyRegistry registry) {
